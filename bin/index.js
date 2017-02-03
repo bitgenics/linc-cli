04f3434db86903b5ca9747e40ef511f295032b10
Sander Huijsen <sander.huijsen@gmail.com>
2017-02-03 17:34:58 +0800

    Renamed username/password to accessKey/secretKey. Started working on 'add user' functionality.

--- bin/index.js
+++ bin/index.js
@@ -2,10 +2,12 @@
 const yargs = require('yargs');
 const login = require('../lib/login/');
 const deploy = require('../lib/deploy/');
+const user = require('../lib/user/');
 
 const argv = yargs
-    .command("login", "Log in", {}, argv => login(false))
+    .command("adduser", "Add a user.", {}, argv => user.add())
     .command("deploy", "Deploy a property.", {}, argv => deploy())
+    .command("login", "Log in.", {}, argv => login(false))
     .demand(1)
     .help("h")
     .alias("h", "help")
