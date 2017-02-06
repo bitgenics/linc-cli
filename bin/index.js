0d9c60fa0e340e6353991c1c969b125ae1a26d88
Erwin van der Koogh <erwin@bitgenics.io>
2017-02-06 12:55:10 +1100

    Added `linc build` and `linc serve`

--- bin/index.js
+++ bin/index.js
@@ -4,11 +4,15 @@ const yargs = require('yargs');
 const login = require('../lib/login/');
 const deploy = require('../lib/deploy/');
 const user = require('../lib/user/');
+const build = require('../lib/build');
+const serve = require('../lib/serve');
 
 const argv = yargs
     .command("adduser", "Add a user.", {}, argv => user.add())
     .command("deploy", "Deploy a property.", {}, argv => deploy())
     .command("login", "Log in.", {}, argv => login(false))
+    .command("build", "Build a SSR package", {}, argv => build())
+    .command("serve", "Run a HTTP server with SSR", {}, argv => serve())
     .demand(1)
     .help("h")
     .alias("h", "help")
