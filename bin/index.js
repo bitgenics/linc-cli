0e812c7750a318ca932736b6bd85f7cd1f100e9a
Sander Huijsen <sander.huijsen@gmail.com>
2017-02-07 13:58:26 +0800

    Added new command addsite. User must be logged in for this to work. Also, command in main file are now alphabetically ordered.

--- bin/index.js
+++ bin/index.js
@@ -4,15 +4,17 @@ const yargs = require('yargs');
 const login = require('../lib/login/');
 const deploy = require('../lib/deploy/');
 const user = require('../lib/user/');
+const site = require('../lib/site/');
 const build = require('../lib/build');
 const serve = require('../lib/serve');
 
 const argv = yargs
+    .command("addsite", "Add a site.", {}, argv => site.add())
     .command("adduser", "Add a user.", {}, argv => user.add())
+    .command("build", "Build a SSR package.", {}, argv => build())
     .command("deploy", "Deploy a site.", {}, argv => deploy())
     .command("login", "Log in.", {}, argv => login(false))
-    .command("build", "Build a SSR package", {}, argv => build())
-    .command("serve", "Run a HTTP server with SSR", {}, argv => serve())
+    .command("serve", "Run a HTTP server with SSR.", {}, argv => serve())
     .demand(1)
     .help("h")
     .alias("h", "help")
