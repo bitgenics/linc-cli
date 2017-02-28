7c64cc6f63e7fb2cf71bb4d3f0c423e313bc8e47
Sander Huijsen <sander.huijsen@gmail.com>
2017-03-01 10:28:12 +1100

    Some code cleanup. Removed log message, which interferes with user prompt.

--- src/cmds/site.js
+++ src/cmds/site.js
@@ -1,6 +1,6 @@
-exports.command = 'site <command>'
-exports.desc = 'Create and manage your LINC sites'
+exports.command = 'site <command>';
+exports.desc = 'Create and manage your LINC sites';
 exports.builder = function (yargs) {
   return yargs.commandDir('site_cmds')
-}
-exports.handler = function (argv) { console.log(argv); }
+};
+exports.handler = function (argv) {};
