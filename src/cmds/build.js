66bda7431b19a5c25923af16bc8470050899d905
Sander Huijsen <sander.huijsen@gmail.com>
2017-03-01 10:58:17 +1100

    Added some messages for the user. Just to be nice.

--- src/cmds/build.js
+++ src/cmds/build.js
@@ -1,19 +1,19 @@
 'use strict';
 
-
-
 const build = () => {
     const path = require('path');
     const buildssr = require('@bitgenics/linc-build-ssr');
     const packageJson = require(path.resolve(process.cwd(), 'package.json'));
     buildssr({}, packageJson, (err, results) => {
     	if (err) console.log(err);
-    	console.log(results);
+
+        console.log('Done! The new build can be found in the folder \'dist\'.');
     });
 };
 
 exports.command = 'build';
 exports.desc = 'Build & package a site for deployment';
 exports.handler = (argv) => {
+    console.log('Building. Please wait...');
 	build();
-}
+};
