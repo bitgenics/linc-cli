c5de8670dbeaeadc26a209009ef7b1e9fdd4150f
Sander Huijsen <sander.huijsen@gmail.com>
2017-03-01 10:26:16 +1100

    Just some code clean up.

--- src/cmds/user.js
+++ src/cmds/user.js
@@ -1,6 +1,6 @@
-exports.command = 'user <command>'
-exports.desc = 'Manage your user settings'
+exports.command = 'user <command>';
+exports.desc = 'Manage your user settings';
 exports.builder = function (yargs) {
   return yargs.commandDir('user_cmds')
-}
-exports.handler = function (argv) {}
+};
+exports.handler = function (argv) {};
