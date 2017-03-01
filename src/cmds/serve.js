3b2333152a59a380500e649e14cef7d6f4ce193d
Sander Huijsen <sander.huijsen@gmail.com>
2017-03-01 11:51:25 +1100

    Retrieve settings from local project's site-settings.json.

--- src/cmds/serve.js
+++ src/cmds/serve.js
@@ -2,6 +2,13 @@
 const path = require('path');
 const fs = require('fs');
 
+const getOptions = () => {
+    const settingsFile = path.join(process.cwd(), 'site-settings.json');
+    const settings = fs.readFileSync(settingsFile, {encoding: 'utf-8'});
+    const jsonObj = JSON.parse(settings);
+    return {settingsVariable: jsonObj.SettingsVariableName, settings: jsonObj.Settings};
+};
+
 const serve = (argv) => {
     const express = require('express');
     const ssr = require('@bitgenics/linc-simple-express');
@@ -9,7 +16,7 @@ const serve = (argv) => {
     const app = express();
 
     const libDir = path.resolve(process.cwd(), 'dist/lib/');
-    const options = {settingsVariable: argv.settingsVariable, settings: argv.site.environments.prod};
+    const options = getOptions();
     app.use('/_assets', express.static(path.resolve(process.cwd(), 'dist/_assets')));
     app.use('/', ssr(libDir, options));
 
@@ -26,4 +33,4 @@ exports.command = 'serve';
 exports.desc = 'Run a Linc server locally';
 exports.handler = (argv) => {
     serve(argv);
-}
+};
