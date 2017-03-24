c384d2b5337568886d89a0ddb7eca037638ad162
Sander Huijsen <sander.huijsen@gmail.com>
2017-03-24 12:33:25 +0800

    New command: linc domain list

---
+++ src/cmds/domain_cmds/list.js
@@ -0,0 +1,77 @@
+'use strict';
+const colors = require('colors/safe');
+const prompt = require('prompt');
+const request = require('request');
+const auth = require('../../auth');
+
+const LINC_API_SITES_ENDPOINT = 'https://aduppa8es1.execute-api.us-west-2.amazonaws.com/v0/sites';
+
+const askSiteName = () => new Promise((resolve, reject) => {
+    let schema = {
+        properties: {
+            site_name: {
+                // Only a-z, 0-9 and - are allowed. Must start with a-z.
+                pattern: /^[a-z]+[a-z0-9-]*$/,
+                description: colors.green('Name of site to release:'),
+                required: true
+            }
+        }
+    };
+    prompt.message = colors.magenta('(linc) ');
+    prompt.delimiter = '';
+    prompt.start();
+    prompt.get(schema, (err, result) => {
+        if (err) return reject(err);
+        else return resolve(result);
+    })
+});
+
+const getAvailableDomains = (site_name, authInfo) => new Promise((resolve, reject) => {
+    console.log('Please wait...');
+    const options = {
+        method: 'GET',
+        url: `${LINC_API_SITES_ENDPOINT}/${site_name}/domains`,
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${authInfo.jwtToken}`
+        }
+    };
+    request(options, (err, response, body) => {
+        if (err) return reject(err);
+
+        const json = JSON.parse(body);
+        if (json.error) return reject(json.error);
+        if (json.domains.length === 0) return reject('No domains available. Add a domain first using \'linc domain add\'.');
+
+        return resolve(json);
+    });
+});
+
+const showAvailableDomains = (results) => {
+    const domains = results.domains;
+    const site_name = results.site_name;
+
+    console.log(`Here are the most recent domains for ${site_name}:`);
+    domains.forEach(d => {
+        console.log(`  +- ${d.domain_name}`);
+        console.log(`        +- Created at ${d.created_at}`);
+    });
+    console.log('');
+};
+
+const list = (argv) => {
+    askSiteName()
+        .then(result => {
+            const siteName = result.site_name.trim();
+            return auth(argv.accessKey, argv.secretKey)
+                .then(auth_params => getAvailableDomains(siteName, auth_params))
+        })
+        .then(result => showAvailableDomains(result))
+        .catch(err => console.log(`Oops, something went wrong: ${err}.`));
+};
+
+exports.command = 'list';
+exports.desc = 'List available domain names';
+exports.handler = (argv) => {
+    list(argv);
+};
