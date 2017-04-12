8dbc76e771b74790a8093319197943f33dbf27da
Sander Huijsen <sander.huijsen@gmail.com>
2017-04-12 17:09:50 +1000

    Added comment block to describe what needs to get done. :)

--- src/cmds/init.js
+++ src/cmds/init.js
@@ -1,5 +1,26 @@
 'use strict';
 
+/**
+ *
+ * @param argv
+ *
+ * Check existence of package.json (mandatory)
+ * Ask user for site name
+ * Ask user for HTTP[S] options
+ * Ask user for profile (e.g., react)
+ * Ask user for domain names (zero or more)
+ *
+ * Add profile package to package.json (for installing)
+ * Add src-dir to linc section in package.json
+ *
+ * Install (npm / yarn)
+ *
+ * Create site
+ * Create error pages (placeholders)
+ *
+ * Write sample linc[.server].config.js
+ *
+ */
 const initialise = (argv) => {
 
 };
