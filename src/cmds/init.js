a1fad1912ae3e7d97842e273b59ec29d5d68c696
Sander Huijsen <sander.huijsen@gmail.com>
2017-04-12 16:40:07 +1000

    Add init sub command.

---
+++ src/cmds/init.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const initialise = (argv) => {
+
+};
+
+
+exports.command = 'init';
+exports.desc = 'Initialise a LINC site';
+exports.handler = (argv) => {
+    initialise(argv);
+};
