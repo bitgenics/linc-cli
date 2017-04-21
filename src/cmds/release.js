f645c79064d4ff89eb9a92f3d8c6a32259c937ec
Sander Huijsen <sander.huijsen@gmail.com>
2017-04-21 12:11:23 +0800

    Allow multiple domains to be released simultaneously.

--- src/cmds/release.js
+++ src/cmds/release.js
@@ -28,6 +28,10 @@ const askDeploymentKey = () => new Promise((resolve, reject) => {
 });
 
 const askReleaseDomain = () => new Promise((resolve, reject) => {
+    console.log(`You can select one or more domains to release (using the same deployment key).
+Simply type the letters associated with all the domains you want to release,
+like so: A B E. Simply press \<Enter\> if you want to release the most recently
+added domain name.`);
     let schema = {
         properties: {
             domain_name_index: {
@@ -57,7 +61,7 @@ const getAvailableDeployments = (site_name, authInfo) => new Promise((resolve, r
         if (err) return reject(err);
 
         const json = JSON.parse(body);
-        if (json.error) return reject(json.error);
+        if (json.error) return reject(new Error(json.error));
         else if (response.statusCode !== 200) return reject(new Error(`Error ${response.statusCode}: ${response.statusMessage}`));
         else if (json.deployments.length === 0) return reject(new Error('No deployments available. Deploy your site using \'linc deploy\'.'));
         else return resolve(json);
@@ -137,9 +141,9 @@ const release = (argv) => {
 
     console.log('Please wait...');
 
+    let domainsToRelease = [];
     let siteName = argv.siteName;
     let authParams = null;
-    let domainName = null;
     let deployKey = null;
     auth(argv.accessKey, argv.secretKey)
         .then(auth_params => {
@@ -149,33 +153,42 @@ const release = (argv) => {
         .then(result => {
             showAvailableDomains(result);
             return askReleaseDomain()
-                .then(answer => {
-                    let index = answer.domain_name_index.substr(0, 1).charCodeAt(0) - 65;
-                    if (index > result.domains.length-1) {
-                        throw new Error('Invalid response. Aborted by user.');
-                    }
-                    domainName = result.domains[index].domain_name;
+                .then(reply => {
+                    let ignored = false;
+
+                    // Convert to uppercase, remove non A-Z characters and remove duplicates
+                    const answers = reply.domain_name_index
+                        .toUpperCase().replace(/[^A-Z]/, '')
+                        .split('').filter((t,i,a) => a.indexOf(t) === i);
+
+                    answers.forEach(a => {
+                        const index = a.charCodeAt(0) - 65;
+                        if (index > result.domains.length - 1) {
+                            if (!ignored) {
+                                ignored = true;
+                                console.log('One or more invalid responses ignored.');
+                            }
+                        } else {
+                            domainsToRelease.push(result.domains[index].domain_name);
+                        }
+                    });
                 })
         })
         .then(() => getAvailableDeployments(siteName, authParams))
         .then(result => {
             showAvailableDeployments(result);
             return askDeploymentKey(true)
-                .then(answer => {
-                    let index = answer.deploy_key_index.substr(0, 1).charCodeAt(0) - 65;
-                    if (index > answer.deploy_key_index-1) {
+                .then(reply => {
+                    let index = reply.deploy_key_index.substr(0, 1).charCodeAt(0) - 65;
+                    if (index > result.deployments.length - 1) {
                         throw new Error('Invalid response. Aborted by user.');
                     }
                     deployKey = result.deployments[index].deploy_key;
+                    console.log('Please wait...');
                 })
         })
-        .then(() => createNewRelease(siteName, deployKey, domainName, authParams))
-        .then(response => {
-            console.log('Release successfully created.');
-            if (response.endpoint !== undefined) {
-                console.log(`Please be reminded the domain name for your site is:\n     ${response.endpoint}.`);
-            }
-        })
+        .then(() => Promise.all(domainsToRelease.map(d => createNewRelease(siteName, deployKey, d, authParams))))
+        .then(response => console.log('Release(s) successfully created.'))
         .catch(err => error(err));
 };
 
