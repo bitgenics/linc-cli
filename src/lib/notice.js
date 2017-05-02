0eea519f13181338e3cd91101050e9f51b7d8e34
Sander Huijsen <sander.huijsen@gmail.com>
2017-05-02 20:08:07 +0800

    Added 'beta' notice to notice.

--- src/lib/notice.js
+++ src/lib/notice.js
@@ -2,7 +2,7 @@
 
 module.exports = () => {
     console.log(`
-!! If you experience any problems or have any questions, !! 
-!! please email us at help@bitgenics.io.                 !!
+!! LINC is still in beta. If you experience any problems, or !!
+!! have any questions, please email us at help@bitgenics.io. !!
 `);
 };
