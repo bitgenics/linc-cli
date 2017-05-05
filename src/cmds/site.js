d11d8c18d64ff43b666814f878d6aa31fb40a197
Sander Huijsen <sander.huijsen@gmail.com>
2017-05-05 12:52:43 +0800

    Removed now superfluous site command file.

--- src/cmds/site.js
+++
@@ -1,6 +0,0 @@
-exports.command = 'site <command>';
-exports.desc = 'Create and manage your LINC sites';
-exports.builder = function (yargs) {
-  return yargs.commandDir('site_cmds')
-};
-exports.handler = function (argv) {};
