0ee348e4b1874f6fc5d882f5b8dd2aa5f9a9b463
Sander Huijsen <sander.huijsen@gmail.com>
2017-09-22 10:23:54 +0800

    Removed beta message from notice.

--- src/lib/notice.js
+++ src/lib/notice.js
@@ -2,7 +2,7 @@
 
 module.exports = () => {
     console.log(`
-!! LINC is still in beta. If you experience any problems, or !!
-!! have any questions, please email us at help@bitgenics.io. !!
+If you experience any problems or have any questions 
+while using LINC, please email us at help@bitgenics.io.
 `);
 };
