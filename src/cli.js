7c5e39139bcc5fb608e28f1720ce85da7e61eda3
Sander Huijsen <sander.huijsen@gmail.com>
2017-11-22 11:35:33 +0800

    Refactored cli.js. Removed some superfluous code.

--- src/cli.js
+++ src/cli.js
@@ -2,19 +2,20 @@
 'use strict';
 const fs = require('fs');
 const yargs = require('yargs');
+const fileCredentials = require('./cred.js').getCredentials();
+
+// Get LINC specific variables from environment
+const linc_keys = Object.keys(process.env).filter((key) => key.startsWith('LINC_'));
+const envConfig = {};
+linc_keys.forEach((key) => envConfig[key.toLowerCase().substring(5)] = process.env[key]);
+
+// Create config object to pass into yargs
+const configObject = Object.assign({}, fileCredentials, envConfig);
 
 const argv = yargs
     .commandDir('cmds')
-    .config('config', 'Config file name', () => {
-        const fileCredentials = require('./cred.js').getCredentials();
-        const linc_keys = Object.keys(process.env).filter((key) => key.startsWith('LINC_'));
-        const envConfig = {};
-        linc_keys.forEach((key) => envConfig[key.toLowerCase().substring(5)] = process.env[key]);
-        return Object.assign({}, fileCredentials, envConfig);
-    })
+    .config(configObject)
     .pkgConf('linc', process.cwd())
-    .global(['config', 'silent'])
-    .default({config: 'linc.toml', silent: false})
     .demand(1)
     .help("help")
     .alias("h", "help")
