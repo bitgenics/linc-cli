7ecdee78a5132792469031e246ca24ff63860cb4
Sander Huijsen <sander.huijsen@gmail.com>
2017-12-20 16:41:32 +0800

    Changed initial timeout to 4 seconds, so it will probably almost always hit within the first waiting time.

--- src/cmds/publish.js
+++ src/cmds/publish.js
@@ -367,7 +367,7 @@ const retrieveDeploymentStatus = (siteName, authInfo) => new Promise((resolve, r
  */
 const waitForDeployToFinish = (envs, siteName, authInfo) => new Promise((resolve, reject) => {
     const Count = 20;
-    let Timeout = 3;
+    let Timeout = 4;
 
     /**
      * Check whether deploy has finished
@@ -383,7 +383,7 @@ const waitForDeployToFinish = (envs, siteName, authInfo) => new Promise((resolve
                 .then(s => {
                     if (s.length === envs.length) return resolve(s);
 
-                    Timeout = 6;
+                    Timeout = 8;
                     return checkForDeployToFinish(count - 1);
                 })
                 .catch(err => reject(err));
