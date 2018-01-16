0a1c0c7faa26b8996d46e4e8762ede17624c7a9b
Sander Huijsen <sander.huijsen@gmail.com>
2018-01-16 09:05:34 +0800

    Utility function.

---
+++ src/lib/dot-linc.js
@@ -0,0 +1,8 @@
+const fs = require('fs-extra');
+const path = require('path');
+
+const DOT_LINC_DIR = path.join(process.cwd(), '.linc')
+
+module.exports.DOT_LINC_DIR = DOT_LINC_DIR;
+
+module.exports.ensureDir = () => fs.ensureDir(DOT_LINC_DIR);
